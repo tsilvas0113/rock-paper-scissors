420cd3d79c370bd285523a92dcd5a79c978f3e01
Tristan <tsilvas0113@gmail.com>
2022-06-01 17:32:33 -0500

    Create function that plays a single round of the game

--- javascript.js
+++ javascript.js
@@ -13,3 +13,42 @@ const computerPlay = () => {
     };
 };
 
+const playRound = (playerSelection, computerSelection) => {
+    playerSelection = playerSelection.toLowerCase();
+    switch (computerSelection) {
+        case 'Rock':
+            if (playerSelection === 'scissors') {
+                return 'You Lose! Rock beats Scissors'
+            } else if (playerSelection === 'paper') {
+                return 'You Win! Paper beats Rock'
+            } else if (playerSelection === 'rock') {
+                return 'Draw! Both selections are Rock'
+            } else {
+                return 'Not a valid choice, choose Rock, Paper, or Scissors'
+            }
+            break;
+        case 'Paper':
+            if (playerSelection === 'scissors') {
+                return 'You Win! Scissors beats Paper'
+            } else if (playerSelection === 'paper') {
+                return 'Draw! Both selections are Paper'
+            } else if (playerSelection === 'rock') {
+                return 'You Lose! Paper beats Rock'
+            } else {
+                return 'Not a valid choice, choose Rock, Paper, or Scissors'
+            }
+            break;
+        case 'Scissors':
+            if (playerSelection === 'scissors') {
+                return 'Draw! Both selections are Scissors'
+            } else if (playerSelection === 'paper') {
+                return 'You Lose! Scissors beats Paper'
+            } else if (playerSelection === 'rock') {
+                return 'You Win! Rock beats Scissors'
+            } else {
+                return 'Not a valid choice, choose Rock, Paper, or Scissors'
+            }
+            break;
+    };
+};
+
